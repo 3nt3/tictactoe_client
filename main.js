baf62c91e022607e932d1e77245e91090a338b8f
Sheesher <sheeshkroete44@gmail,c>
2018-11-10 17:22:41 +0100

    Add some more Hydrauliknotdienst

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var rootUrl = "http://127.0.0.1:8000"
 function getStates(room) {
 	var state;
 	xhttp = new XMLHttpRequest();
-	var url = rootUrl+"/tictactoe/"+room+"/getState/"
+	var url = rootUrl+"/tictactoe/"+room+"/getState"
 	xhttp.open("GET", url, true);
 	xhttp.onreadystatechange = function() {
   		if (this.readyState == 4 && this.status == 200) {
@@ -19,6 +19,7 @@ function getStates(room) {
 }
 
 function updateState(room, n, state) {
+	var url = rootUrl+"/tictactoe/"+room+"/updateState/"+n 
 	xhttp = new XMLHttpRequest();
 	xhttp.open("POST", rootUrl+"/tictactoe/{0}/updateState/{1}".format(room, n));
 	xhttp.send(state);
@@ -34,9 +35,29 @@ function createRoom(players) {
 }
 
 function getScores(room) {
+	var scores;
+	var url = rootUrl+"/tictactoe/"+room+"/getScores";
+	xhttp = new XMLHttpRequest();
 
+	xhttp.open("GET", url, true);
+	xhttp.onreadystatechange = function() {
+  		if (this.readyState == 4 && this.status == 200) {
+    		scores = this.responseText;
+  		}
+  	};
+
+  	return scores.toString();
 }
 
-function updateScore(room, player) {
+function updateScore(room, player, score) {
+	var url = rootUrl+"/tictactoe/"+room+"/updateScore/"+player;
 
+	xhttp = new XMLHttpRequest();
+	xhttp.open("POST", url);
+	xhttp.send(score)
 }
+
+function keksen() {
+	
+}
+
